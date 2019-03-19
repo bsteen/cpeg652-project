497d13b29ff67eb726266781e331ff6799016cf3
bsteen <benjamin@localdomain>
2019-03-19 02:52:41 -0400

    Removed random generation test

--- nbody/nbody_seq.c
+++ nbody/nbody_seq.c
@@ -11,7 +11,7 @@
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -20,9 +20,9 @@
  * 02110-1301 USA.
  */
 
-
-/* Name: nbody_seq.c: sequential 2-d nbody simulation
- */
+// Name: nbody_seq.c: sequential 2-d nbody simulation
+// Edits done by: Benjamin Steenkamer, 2019
+ 
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -33,40 +33,38 @@
 #define PWIDTH 1
 
 /* There is one structure of this type for each "body" in the
- * simulation.  All of the attributes and the current state of the
+ * simulation.	All of the attributes and the current state of the
  * body are recorded in this structure. */
 typedef struct BodyStruct {
-  double mass;     /* mass of body */
-  int color;       /* color used to draw this body */
-  int size;        /* diameter of body in pixels */
-  double x;        /* x position */
-  double y;        /* y position */
-  double vx;       /* velocity, x-direction */
-  double vy;       /* velocity, y-direction */
+  double mass;	   /* mass of body */
+  int color;	   /* color used to draw this body */
+  int size;		   /* diameter of body in pixels */
+  double x;		   /* x position */
+  double y;		   /* y position */
+  double vx;	   /* velocity, x-direction */
+  double vy;	   /* velocity, y-direction */
 } Body;
 
 /* Global variables */
-double x_min;        /* coord of left edge of universe */
-double x_max;        /* coord of right edge of universe */
-double y_min;        /* coord of bottom edge of universe */
-double y_max;        /* coord of top edge of universe */
-double univ_x;       /* x_max-x_min */
-double univ_y;       /* y_max-y_min */
-int nx;              /* width of movie window (pixels) */
-int ny;              /* height of movie window (pixels) */
-int numBodies;       /* number of bodies */
-double K;            /* single constant encoding G, grid spacing, etc. */
-int nsteps;          /* number of time steps */
-int period;          /* number of times steps beween movie frames */
-FILE *gif;           /* file containing animated GIF */
-gdImagePtr im,       /* pointers to consecutive GIF images */
-  previm;  
-int *colors;         /* colors we will use */
-Body *bodies,        /* two copies of main data structure: list of bodies */
+double x_min;		 /* coord of left edge of universe */
+double x_max;		 /* coord of right edge of universe */
+double y_min;		 /* coord of bottom edge of universe */
+double y_max;		 /* coord of top edge of universe */
+double univ_x;		 /* x_max-x_min */
+double univ_y;		 /* y_max-y_min */
+int nx;				 /* width of movie window (pixels) */
+int ny;				 /* height of movie window (pixels) */
+int numBodies;		 /* number of bodies */
+double K;			 /* single constant encoding G, grid spacing, etc. */
+int nsteps;			 /* number of time steps */
+int period;			 /* number of times steps beween movie frames */
+FILE *gif;			 /* file containing animated GIF */
+gdImagePtr im,		 /* pointers to consecutive GIF images */
+  previm;
+int *colors;		 /* colors we will use */
+Body *bodies,		 /* two copies of main data structure: list of bodies */
   *bodies_new;
 int byteCount = 0;
-
-
 
 void* my_malloc(int numBytes) {
   void *result = malloc(numBytes);
@@ -136,27 +134,27 @@ void init(char* infilename, char* outfilename) {
   bodies = (Body*)my_malloc(numBodies*sizeof(Body));
   bodies_new = (Body*)my_malloc(numBodies*sizeof(Body));
   for (i=0; i<numBodies; i++) {
-    fscanf(infile, "%lf", &bodies[i].mass);
-    assert(bodies[i].mass > 0);
-    fscanf(infile, "%d", &bodies[i].color);
-    assert(bodies[i].color >=0 && bodies[i].color<MAXCOLORS);
-    fscanf(infile, "%d", &bodies[i].size);
-    assert(bodies[i].size > 0);
-    fscanf(infile, "%lf", &bodies[i].x);
-    assert(bodies[i].x >=x_min && bodies[i].x < x_max);
-    fscanf(infile, "%lf", &bodies[i].y);
-    assert(bodies[i].y >=y_min && bodies[i].y < y_max);
-    fscanf(infile, "%lf", &bodies[i].vx);
-    fscanf(infile, "%lf", &bodies[i].vy);
+	fscanf(infile, "%lf", &bodies[i].mass);
+	assert(bodies[i].mass > 0);
+	fscanf(infile, "%d", &bodies[i].color);
+	assert(bodies[i].color >=0 && bodies[i].color<MAXCOLORS);
+	fscanf(infile, "%d", &bodies[i].size);
+	assert(bodies[i].size > 0);
+	fscanf(infile, "%lf", &bodies[i].x);
+	assert(bodies[i].x >=x_min && bodies[i].x < x_max);
+	fscanf(infile, "%lf", &bodies[i].y);
+	assert(bodies[i].y >=y_min && bodies[i].y < y_max);
+	fscanf(infile, "%lf", &bodies[i].vx);
+	fscanf(infile, "%lf", &bodies[i].vy);
 #ifdef DEBUG
-    printf("Body %d:\n", i);
-    printBody(stdout, &bodies[i]);
+	printf("Body %d:\n", i);
+	printBody(stdout, &bodies[i]);
 #endif
   }
   for (i=0; i<numBodies; i++) {
-    bodies_new[i].mass = bodies[i].mass;
-    bodies_new[i].color = bodies[i].color;
-    bodies_new[i].size = bodies[i].size;
+	bodies_new[i].mass = bodies[i].mass;
+	bodies_new[i].color = bodies[i].color;
+	bodies_new[i].size = bodies[i].size;
   }
   fflush(stdout);
   fclose(infile);
@@ -169,45 +167,45 @@ void write_plain(int time) {
 
   printf("\nTime = %d\n", time);
   for (i=0; i<numBodies; i++) {
-    printf("Body %d:\n", i);
-    printBody(stdout, &bodies[i]);
+	printf("Body %d:\n", i);
+	printBody(stdout, &bodies[i]);
   }
 }
 
 /* Write one frame of the GIF for given time */
 void write_frame(int time) {
   int i;
-  
+
   im = gdImageCreate(nx,ny);
   if (time == 0) {
-    gdImageColorAllocate(im, 0, 0, 0);  /* black background */
-    for (i=0; i<MAXCOLORS; i++)
-      colors[i] = gdImageColorAllocate (im, i, 0, MAXCOLORS-i-1); 
-    /* (im, i,i,i); gives gray-scale image */
-    gdImageGifAnimBegin(im, gif, 1, -1);
+	gdImageColorAllocate(im, 0, 0, 0);	/* black background */
+	for (i=0; i<MAXCOLORS; i++)
+	  colors[i] = gdImageColorAllocate (im, i, 0, MAXCOLORS-i-1);
+	/* (im, i,i,i); gives gray-scale image */
+	gdImageGifAnimBegin(im, gif, 1, -1);
   } else {
-    gdImagePaletteCopy(im, previm);
+	gdImagePaletteCopy(im, previm);
   }
   for (i=0; i<numBodies; i++) {
-    Body *body = bodies + i;
-    double x = body->x;
+	Body *body = bodies + i;
+	double x = body->x;
 
-    if (x>=0 && x<nx) {
-      double y = body->y;
+	if (x>=0 && x<nx) {
+	  double y = body->y;
 
-      if (y>=0 && y<ny) {
+	  if (y>=0 && y<ny) {
 	int size = bodies[i].size;
 	int color = bodies[i].color;
 
 	gdImageFilledEllipse(im, (int)x, ny-(int)y, size, size, colors[color]);
-      }
-    }
+	  }
+	}
   }
   if (time == 0) {
-    gdImageGifAnimAdd(im, gif, 0, 0, 0, 0, gdDisposalNone, NULL);
+	gdImageGifAnimAdd(im, gif, 0, 0, 0, 0, gdDisposalNone, NULL);
   } else {
-    gdImageGifAnimAdd(im, gif, 0, 0, 0, 5, gdDisposalNone, /* previm */ NULL);
-    gdImageDestroy(previm);
+	gdImageGifAnimAdd(im, gif, 0, 0, 0, 5, gdDisposalNone, /* previm */ NULL);
+	gdImageDestroy(previm);
   }
   previm=im;
   im=NULL;
@@ -216,9 +214,9 @@ void write_frame(int time) {
 #endif
 }
 
-/* Move forward one time step.  This is the "integration step".  For
+/* Move forward one time step.	This is the "integration step".	 For
  * each body b, compute the total force acting on that body.  If you
- * divide this by the mass of b, you get b's acceleration.  So you
+ * divide this by the mass of b, you get b's acceleration.	So you
  * actually just calculate the b's acceleration directly, since this
  * is what you want to know.  Once you have the acceleration, proceed
  * as follows: update the position by adding the current velocity,
@@ -229,42 +227,42 @@ void update() {
   Body *tmp;
 
   for (i=0; i<numBodies; i++) {
-    double x = bodies[i].x;
-    double y = bodies[i].y;
-    double vx = bodies[i].vx;
-    double vy = bodies[i].vy;
-    double ax = 0;
-    double ay = 0;
+	double x = bodies[i].x;
+	double y = bodies[i].y;
+	double vx = bodies[i].vx;
+	double vy = bodies[i].vy;
+	double ax = 0;
+	double ay = 0;
 
-    for (j=0; j<numBodies; j++) {
-      double r, mass, dx, dy, r_squared, acceleration;
+	for (j=0; j<numBodies; j++) {
+	  double r, mass, dx, dy, r_squared, acceleration;
 
-      if (j==i) continue;
-      dx = bodies[j].x - x;
-      dy = bodies[j].y - y;
-      mass = bodies[j].mass;
-      r_squared = dx*dx + dy*dy;
-      if (r_squared != 0) {
+	  if (j==i) continue;
+	  dx = bodies[j].x - x;
+	  dy = bodies[j].y - y;
+	  mass = bodies[j].mass;
+	  r_squared = dx*dx + dy*dy;
+	  if (r_squared != 0) {
 	r = sqrt(r_squared);
 	if (r != 0) {
 	  acceleration = K*mass/(r_squared);
 	  ax += acceleration*dx/r;
 	  ay += acceleration*dy/r;
 	}
-      }
-    }
-    x += vx;
-    y += vy;
-    if (x>=x_max || x<x_min) x=x+(ceil((x_max-x)/univ_x)-1)*univ_x;
-    if (y>=y_max || y<y_min) y=y+(ceil((y_max-y)/univ_y)-1)*univ_y;
-    vx += ax;
-    vy += ay;
-    assert(!(isnan(x) || isnan(y)));
-    assert(!(isnan(vx) || isnan(vy)));
-    bodies_new[i].x = x;
-    bodies_new[i].y = y;
-    bodies_new[i].vx = vx;
-    bodies_new[i].vy = vy;
+	  }
+	}
+	x += vx;
+	y += vy;
+	if (x>=x_max || x<x_min) x=x+(ceil((x_max-x)/univ_x)-1)*univ_x;
+	if (y>=y_max || y<y_min) y=y+(ceil((y_max-y)/univ_y)-1)*univ_y;
+	vx += ax;
+	vy += ay;
+	assert(!(isnan(x) || isnan(y)));
+	assert(!(isnan(vx) || isnan(vy)));
+	bodies_new[i].x = x;
+	bodies_new[i].y = y;
+	bodies_new[i].vx = vx;
+	bodies_new[i].vy = vy;
   }
   tmp = bodies;
   bodies = bodies_new;
@@ -281,91 +279,52 @@ void wrapup() {
   free(bodies_new);
 }
 
-/* return random int in range [a,b) */
-int randomInt(int a, int b) {
-  double r = ((double)rand()/((double)(RAND_MAX)+(double)(1))); /* [0,1) */
-  double x = a+r*(b-a); /* [a,b) */
-  int result = (int)x;
-
-  return result;
-}
-
-/* This is an alternative to the initialization by reading config
- * file.  This function instead creates 500 random bodies within
- * certain parameters.
- */
-void randinit(char *outfilename) {
-  int i;
-
-  x_min =-1000;
-  x_max = 3000;
-  y_min =-600;
-  y_max = 1200;
-  univ_x = x_max-x_min;
-  univ_y = y_max-y_min;
-  nx = 1000;
-  ny = 600;
-  K = .15;
-  nsteps = 1500;
-  period = 2;
-  numBodies = 500;
-  bodies = (Body*)my_malloc(numBodies*sizeof(Body));
-  bodies_new = (Body*)my_malloc(numBodies*sizeof(Body));
-  for (i=0; i<numBodies; i++) {
-    bodies[i].mass = randomInt(1,8);
-    bodies[i].color = randomInt(0,253);
-    bodies[i].size = bodies[i].mass;
-    bodies[i].x = randomInt(x_min, x_max);
-    bodies[i].y = randomInt(y_min, y_max);
-    bodies[i].vx = randomInt(-1,1)/4;
-    bodies[i].vy = randomInt(-1,1)/4;
-    assert(!(isnan(bodies[i].x) || isnan(bodies[i].y) ||
-	     isnan(bodies[i].vx) || isnan(bodies[i].vy)));
-  }
-  for (i=0; i<numBodies; i++) {
-    bodies_new[i].mass = bodies[i].mass;
-    bodies_new[i].color = bodies[i].color;
-    bodies_new[i].size = bodies[i].size;
-  }
-  prepgif(outfilename);
-}
-
-/* Perform an n-body simulation and create a GIF movie.  Usage: you
+/* Perform an n-body simulation and create a GIF movie.	 Usage: you
  * can either specify two arguments: the name of the configuration
  * file and the name of the GIF file you are going to create, or you
  * can specify one argument (just the name of the GIF file), in which
  * case random initialization is used. */
-int main(int argc, char* argv[]) {
-  int i;
-  clock_t t0 = clock();
-  clock_t t1;
+int main(int argc, char* argv[]) 
+{
+	int i;
+	clock_t t0 = clock();
+	clock_t t1;
 
-  if (argc == 2) {
-    randinit(argv[1]);
-  } else {
-    if (argc != 3) {
-      printf("Usage: nbody <infilename> <outfilename>\n");
-      exit(1);
-    }
-    init(argv[1], argv[2]);
-  }
-  write_frame(0);
-  for (i=1; i<=nsteps; i++) {
-#ifdef DEBUG
-    printf("Computing time %d...", i);
-    fflush(stdout);
-#endif
-    update();
-#ifdef DEBUG
-    printf("done.\n");
-    fflush(stdout);
-#endif
-    if (i%period == 0) write_frame(i);
-  }
-  wrapup();
-  t1 = clock()-t0;
-  printf("Total time (seconds): %f\n", 1.0*t1/CLOCKS_PER_SEC);
-  printf("Total memory allocated (bytes): %d\n", byteCount);
-  fflush(stdout);	 
-  return 0;
+	if (argc != 3) 
+	{
+		printf("Usage: nbody <infilename> <outfilename>\n");
+		exit(1);
+	}
+	
+	init(argv[1], argv[2]);
+	write_frame(0);
+	
+	for (i=1; i<=nsteps; i++)
+	{
+		#ifdef DEBUG
+		printf("Computing time %d...", i);
+		fflush(stdout);
+		#endif
+		
+		update();
+		
+		#ifdef DEBUG
+		printf("done.\n");
+		fflush(stdout);
+		#endif
+		
+		if (i%period == 0)
+		{
+			write_frame(i);
+		}
+	}
+	
+	wrapup();
+	t1 = clock() - t0;
+	
+	printf("Total time (seconds): %f\n", 1.0*t1/CLOCKS_PER_SEC);
+	printf("Total memory allocated (bytes): %d\n", byteCount);
+	fflush(stdout);
+	
+	return 0;
 }
